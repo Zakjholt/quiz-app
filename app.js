f6b73935144739921e340267cd87f75e310ca9b0
zak <zak.j.holt@gmail.com>
2016-08-28 15:43:58 -0400

    finish basic functionality

---
+++ app.js
@@ -0,0 +1,42 @@
+var correctAnswers = 0;
+$(document).ready(function() {
+
+});
+
+//Start button functionality
+$(".startButton").click(function() {
+    $(this).hide();
+    $(".question").first().show();
+});
+
+//Selecting answers functionality
+$(".answers button").click(function() {
+    $(".answers button").removeClass("selected");
+    $(this).addClass("selected");
+});
+
+//When the user clicks a submit button
+$(".submit").click(function() {
+    if ($(this).closest(".question").find("button").hasClass("selected")) {
+        if ($(this).closest(".question").find(".selected").hasClass("correct")) {
+            correctAnswers++;
+            updateAnswers();
+            $(this).closest(".question").hide();
+            $(this).closest(".question").next().show();
+        } else { //The correct answer is highlighted, the submit button is replaced with a continue button
+            $(this).closest(".question").find(".selected").css("background", "red");
+            $(this).closest(".question").find(".correct").css("background", "green");
+            $(this).closest(".question").append("<button class='continue'>Continue ></button>");
+            $(this).hide();
+            $(".continue").click(function() {
+              $(this).closest(".question").hide();
+              $(this).closest(".question").next().show();
+            });
+        }
+    }
+});
+
+
+function updateAnswers() {
+  $(".count").text(correctAnswers);
+}
